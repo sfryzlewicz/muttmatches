714e9645cedf0de2b4021c82735b61c7530cd65c
Sara Fryzlewicz <sfryzlewicz@hawk.iit.edu>
2023-12-08 20:25:03 -0600

    Login UI Changes

--- app/pages/login.jsx
+++ app/pages/login.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React, {useState} from 'react'
-import imageOne from '/app/bestFriend.jpg'
+import imageOne from 'app/getFriend.jpg'
 import { login } from '../services/authService';
 import { Alert, Card, Form, Input, Button, Carousel } from 'antd';
 import Slider from 'react-slick';
@@ -21,13 +21,15 @@ const Login = () => {
   const isLoginDisabled = !name || !email || !isEmailValid;
   const [loading, setLoading] = useState(false);
 
+  console.log("here", imageOne)
 
   const settings = {
-    dots: true,
+    dots: false,
     infinite: true,
-    speed: 500,
+    speed: 15,
     slidesToShow: 1,
     slidesToScroll: 1,
+    marginBottom:16,
   };
 
   const handleClose = () => {
@@ -53,24 +55,33 @@ const Login = () => {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       <Card style={{ width: 400, textAlign: 'center' }}>
-        <Slider {...settings}>
+        <Slider style={{height:'150px', marginLeft:'10px', marginRight:'10px'}}{...settings}>
           <div>
-            <img src={imageOne} alt="Image 1" style={{ width: '100%', height: 'auto' }} />
+            <img src="/_next/static/media/NrOnebestFriend.9a787a6e.jpg" alt="Image 1" style={{ width: 'auto', height: '100%' }} />
           </div>
           <div>
-            <img src={imageOne} alt="Image 2" style={{ width: '100%', height: 'auto' }} />
+            <img src={'/_next/static/media/returnFriend.38bb16f2.jpg'} alt="Image 2" style={{ width: '100%', height: '100%' }} />
+          </div>
+          <div>
+            <img src={'/_next/static/media/chewsFriend.3ee5bbfe.jpg'} alt="Image 3" style={{ width: '100%', height: '100%' }} />
+          </div>
+          <div>
+            <img src={'/_next/static/media/repFriend.fb8b7542.jpg'} alt="Image 4" style={{ width: '100%', height: '100%' }} />
+          </div>
+          <div>
+            <img src={'/_next/static/media/getFriend.43d2431b.jpg'} alt="Image 5" style={{ width: '100%', height: '100%' }} />
           </div>
           {/* Add more divs with images */}
         </Slider>
-        <Form style={{ marginTop: 16 }} initialValues={{ name, email }} onFinish={handleLogin} name="form">
+        <Form style={{ marginTop: 35 }} initialValues={{ name, email }} onFinish={handleLogin} name="form">
         {visible && (
         <Alert message="Please enter a valid email" type="error" closable afterClose={handleClose}/>
       )}
-          <Form.Item label="Name" name="name" rules={[{ required: true, message: 'Please enter your name' }]}>
-            <Input value={name} onChange={(e) => setName(e.target.value)} />
-          </Form.Item>
           <Form.Item label="Email" name="email" rules={[{ required: true, message: 'Please enter your email' }]}>
             <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </Form.Item>
+          <Form.Item label="Password" name="password" rules={[{ required: true, message: 'Please enter your password' }]}>
+            <Input value={name} onChange={(e) => setName(e.target.value)} />
           </Form.Item>
           <Form.Item>
             <Button type="primary" htmlType="submit" disabled={isLoginDisabled}>
